81167d60b0cc9bcbc250c699a1b14343527539cd
Sameer Yasin <113780206+SameerYasin@users.noreply.github.com>
2025-07-24 00:35:27 +0530

    Create Code-To-Check-Odd-Even.cpp

---
+++ Code-To-Check-Odd-Even.cpp
@@ -0,0 +1,25 @@
+// SAMEER YASIN Exp-5
+// 24070123145
+// ENTC B1
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    if (n % 2 == 0){
+        cout << "The number you have entered is EVEN " << endl;
+    } else {
+        cout << "The number you have entered is ODD" << endl;
+    }
+    return 0;
+}
+
+/*Sample Output:
+Enter the number: 64
+The number you have entered is EVEN
+Enter the number: 73
+The number you have entered is ODD
+*/
